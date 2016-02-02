80c91bf5912f27d6f2a77c9d6b6399c408368a2e
18511898052 <15170025944@163.com>
2016-02-02 15:04:50 +0800

    test third

--- test.php
+++ test.php
@@ -3,6 +3,21 @@
 <?php
 
 echo 'hahaha';
-echo 'hellohagfhaf';
+/*
+git remote add develop(本地分支) http://xxxxxxxxxxxx(远程分支地址)
+git push -u origin develop:develop
 
+git push origin --delete develop 删除远程分支
+*/
+//echo 'hellohagfhaf';
+
+/*
+git stash (pop)
+git rebase
+git reset --hard
+git reset --soft key   head移至master版本   将修改撤销保留  
+git pull --rebase origin master
+
+git remote add develop(本地分支) http://xxxxxxxxxxxx(远程分支地址)
+*/
 var_dump('safasf');
