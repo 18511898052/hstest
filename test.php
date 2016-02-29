d4d97b5a59383b102bfaa1e16842ac6d5ad3760c
18511898052 <hs19920301>
2016-02-29 13:34:13 +0800

    1111111

--- test.php
+++ test.php
@@ -4,12 +4,10 @@
 
 echo 'hahaha';
 echo 'ajsfasf';
-/*
 git remote add develop(本地分支) http://xxxxxxxxxxxx(远程分支地址)
 git push -u origin develop:develop
 
 git push origin --delete develop 删除远程分支
-*/
 //echo 'hellohagfhaf';
 
 git stash (pop)
