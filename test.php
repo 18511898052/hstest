58a0e7688c7a4074e5dcfbd76d03a82d2cf01fc8
18511898052 <hs19920301>
2016-03-04 10:15:51 +0800

    5555

--- test.php
+++ test.php
@@ -8,7 +8,10 @@ git remote add develop(本地分支) http://xxxxxxxxxxxx(远程分支地址)
 git push -u origin develop:develop
 
 git push origin --delete develop 删除远程分支
-//echo 'hellohagfhaf';
+echo 'hellohagfhaf';
+
+
+/*git --soft 测试*/
 
 git stash (pop)
 git rebase
